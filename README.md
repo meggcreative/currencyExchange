ca81260b783a4ed9e90d01a4bf09c225db93cae1
Magdalena Zulewska <madziapernach@gmail.com>
2021-04-29 15:02:43 +0200

    added image

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# currency-converter
+# Currency Coverter for YouCode project
+
+## Table of content: 
+* [ Preview the coverter](#currency-converter)
+* [ Basic Information ](#basic-information) 
+* [ Used Technologies](#used-technologies)
+* [Usage](#usage)
+
+## Preview the coverter
+
+https://meggcreative.github.io/currencyExchange/index.html
+
+![currency converter site](images/converter.png)
+## Basic Information
+
+This project was created to make a simple example of using forms in JS. This currency converter shows how many PLN we can have if we have some amount of currency: EUR/USD/GBP.
+
+## Used Technologies
+1. Semantic HTML
+2. CSS
+3. BEM convenction
+4. ES6 features
+
+## Usage
+1. Choose currency which You want to covert to PLN
+2. Enter Your amount 
+3. Press button "Przelicz"
+4. Final converted amount will show on the bottom of page
+
--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 let currencyEUR = document.querySelector(".js-currencyEUR");
 let currencyUSD = document.querySelector(".js-currencyUSD");
 let currencyGBP = document.querySelector(".js-currencyGBP");
-
-let currencyEUROut = document.querySelector(".out-js-currencyEUR");
-let currencyUSDOut = document.querySelector(".out-js-currencyUSD");
-let currencyGBPOut = document.querySelector(".out-js-currencyGBP");
-
 let convertElement = document.querySelector(".js-convert");
 
 let formElement = document.querySelector(".js-form");
@@ -15,57 +10,20 @@ let amountElement = document.querySelector(".js-amount");
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
-    let convertFromPrice = arrayToPrice([currencyEUR, currencyUSD, currencyGBP])
-    let convertToPrice = arrayToPrice([currencyEUROut, currencyUSDOut, currencyGBPOut])
-    let outValue = amountElement.value * convertFromPrice / convertToPrice
 
-    convertFromPrice = magdaFuncInput()
-    let outputCurrency = magdaFuncOutput(convertFromPrice)
-    console.log(outValue.toFixed(2), convertFromPrice)
-        // convertElement.innerText = `${convert.toFixed(2)}`
-
-});
-
-
-function arrayToPrice(inputChecks) {
-    let inputValues = [4.56, 3.78, 5.25]
-    let outputValue = 0
-    for (let i = 0; i < inputChecks.length; i++) {
-        if (inputChecks[i].checked) {
-            outputValue += inputValues[i]
-        }
-    }
-    return outputValue
-}
-
-function magdaFuncInput() {
-    let valueInPLN = null
     switch (true) {
         case currencyEUR.checked:
-            valueInPLN = amountElement.value * 4.56;
+            convert = amountElement.value * 4.56;
+            convertElement.innerText = `${convert.toFixed(2)}`
             break;
         case currencyUSD.checked:
-            valueInPLN = amountElement.value * 3.78;
+            convert = amountElement.value * 3.78;
+            convertElement.innerText = `${convert.toFixed(2)}`
             break;
         case currencyGBP.checked:
-            valueInPLN = amountElement.value * 5.25;
+            convert = amountElement.value * 5.25;
+            convertElement.innerText = `${convert.toFixed(2)}`
             break;
     }
-    return valueInPLN
-}
 
-function magdaFuncOutput(amountOfPLN) {
-    let amountOfCurrency = null
-    switch (true) {
-        case currencyEUROut.checked:
-            amountOfCurrency = amountOfPLN / 4.56
-            break;
-        case currencyUSDOut.checked:
-            amountOfCurrency = amountOfPLN / 3.78
-            break;
-        case currencyGBPOut.checked:
-            amountOfCurrency = amountOfPLN / 5.25
-            break;
-    }
-    return amountOfCurrency
-}
+});
